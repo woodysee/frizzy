525cf3d2f63c55c9b275de3e31e171f6e72ae423
Woody See <woodyseesk@gmail.com>
2018-05-29 21:04:40 +0800

    Completed input radio basic, WIP - input radio binary, WIP - layouts form multistep

--- src/components/addALogoOnTheRight/index.js
+++ src/components/addALogoOnTheRight/index.js
@@ -3,18 +3,20 @@ import './index.css';
 import FrizzyIcon from './icon.png';
 
 export default function addALogoOnTheRight() {
-  const logoWrapper = document.createElement('div');
+  const logoSchema = document.createElement('div');
   const targets = document.getElementsByClassName('add-a-logo-on-the-right');
-  logoWrapper.classList.add("world");
+  logoSchema.classList.add("world");
   const frizzyLogo = new Image();
   frizzyLogo.src = FrizzyIcon;
   frizzyLogo.style.width = "30px";
-  logoWrapper.appendChild(frizzyLogo);
+  logoSchema.appendChild(frizzyLogo);
+  let logoClone;
   if (targets.length > 0) {
-    // console.log("Attaching the Frizzy logo to each element...");
     for (let i = 0; i < targets.length; i++) {
-      targets[i].appendChild(logoWrapper);
+      // console.log("Attaching the Frizzy logo to each element...");
+      logoClone = logoSchema.cloneNode(true);
+      targets[i].appendChild(logoClone);
     }
   }
-  return logoWrapper;
+  return logoSchema;
 }

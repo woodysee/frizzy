7a95abcab114ecb083bd1b5d841c64221907e50f
Woody See <woodyseesk@gmail.com>
2018-05-30 16:32:16 +0800

    added basic form layout logic

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 // import addALogoOnTheRightComponent from './components/addALogoOnTheRight';
 import basicRadioInput from './components/input/radio/basic';
 import binaryRadioInput from './components/input/radio/binary';
+import multiStepFormLayout from './components/layouts/forms/multistep';
 
 // Initialise each imported component on page load
 // addALogoOnTheRightComponent();
 basicRadioInput();
 binaryRadioInput();
+multiStepFormLayout();

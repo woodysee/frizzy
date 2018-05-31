99bd85c39a41f2857321ff31f818b9ebb95222d3
Woody See <woodyseesk@gmail.com>
2018-05-31 11:47:09 +0800

    completed basic multistep form styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // Add all components to be compiled here
 // import addALogoOnTheRightComponent from './components/addALogoOnTheRight';
-import basicRadioInput from './components/input/radio/basic';
-import binaryRadioInput from './components/input/radio/binary';
+import basicRadioInput from './components/inputs/radio/basic';
+import binaryRadioInput from './components/inputs/radio/binary';
 import multiStepFormLayout from './components/layouts/forms/multistep';
+import headerBackBtn from './components/buttons/header/back';
 
 // Initialise each imported component on page load
 // addALogoOnTheRightComponent();
 basicRadioInput();
 binaryRadioInput();
 multiStepFormLayout();
+headerBackBtn();

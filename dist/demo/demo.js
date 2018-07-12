0fceb8ef1bd72f11bf888955a78af0540ce29ed6
Woody See <woodyseesk@gmail.com>
2018-07-12 19:04:58 +0800

    demo page styles

--- dist/demo/demo.js
+++ dist/demo/demo.js
@@ -53,3 +53,22 @@
   wrapperEl.addEventListener("click", printValueOfSelected);
   
 })();
+
+(() => {
+  const selectors = document.getElementsByName("tickedRadioInputs");
+  const printTargetEl = document.getElementById("console--input-radio-ticked");
+  const wrapperEl = document.getElementById("demo-container--input-radio-ticked");
+  
+  function printValueOfSelected () {
+    for (let i = 0; i < selectors.length; i++) {
+  		if (selectors[i].checked) {
+  			printTargetEl.innerHTML = "&nbsp;&gt;&nbsp;" + selectors[i].value;
+  		}
+  	}
+  }
+  
+  printValueOfSelected();
+  
+  wrapperEl.addEventListener("click", printValueOfSelected);
+  
+})();

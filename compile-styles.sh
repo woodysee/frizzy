77260f72470c2080f5c36a751834c8b163efd90a
Woody See <woodyseesk@gmail.com>
2018-07-13 22:28:32 +0800

    reference adding

--- compile-styles.sh
+++ compile-styles.sh
@@ -1,7 +1,7 @@
 # Compile all CSS files into single file
 cd src
 find . -name '*.css'
-cat `find . -name '*.css' -print` > ../dist/frizzy.css
+cat `find . -name '*.css' -print` > ../dist/frizzy.css # Reference: https://stackoverflow.com/questions/864316/how-to-pipe-list-of-files-returned-by-find-command-to-cat-to-view-all-the-files
 echo "...frizzy CSS compiled."
 cd ..
 node minify-styles.js

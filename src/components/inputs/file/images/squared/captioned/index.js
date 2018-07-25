24586b7496b4741a36135aa01bc209c763c28b89
Woody See <woodyseesk@gmail.com>
2018-07-25 21:14:51 +0800

    added optional feature where multiple image uploader can be added one at a time with maximum upload slots specified

--- src/components/inputs/file/images/squared/captioned/index.js
+++ src/components/inputs/file/images/squared/captioned/index.js
@@ -1,8 +1,7 @@
 import './index.css';
 
 export default function captionedImagesFileInput () {
-  const captionedImageUploadSlots = document.querySelectorAll('.fz-uploader[data-fz-uploader-file-type="image"][data-fz-uploader-variant="squared__captioned"]');
-  console.log(captionedImageUploadSlots);
+  const captionedImageUploadSlotsEls = document.querySelectorAll('.fz-uploader[data-fz-uploader-file-type="image"][data-fz-uploader-variant="squared__captioned"]');
   function detectRemovalOfUploadedImage (evt) {
     let previewImgGrpEl, removeImgBtnEl;
     if (evt.target.classList.contains('fz-upload-slot__rm-img__icon')) {
@@ -24,8 +23,14 @@ export default function captionedImagesFileInput () {
         '<div class="fz-upload-slot__rm-img__icon"></div>'+
       '</button>'
     );
+    const slotEl = previewImgGrpEl.parentNode;
+    const slotsEl = slotEl.parentNode.parentNode;
+    if (typeof slotsEl.dataset.fzMaxUploadSlots !== 'undefined') {
+      removeImageUploadSlot(slotsEl, slotEl);
+      slotsEl.addEventListener('change', detectChangesOfUploadedImages);
+    }
   }
-  
+
   function detectChangesOfUploadedImages (evt) {
     const previewWrapperEl = evt.target.parentNode.getElementsByClassName('fz-upload-slot__preview-grp')[0];
     const previewImageEl = evt.target.parentNode.querySelector('.fz-upload-slot__preview-wrapper img');
@@ -42,9 +47,8 @@ export default function captionedImagesFileInput () {
         previewImageEl.style.width = "100%";
       }
     };
+    if (typeof evt.target.files === 'undefined' || typeof evt.target.files[0] === 'undefined') return;
     reader.readAsDataURL(evt.target.files[0]);
-    
-    const errorMessageEl = document.querySelector('.fz-uploader__errors');
     
     const removeUploadedImageBtns = previewWrapperEl.getElementsByClassName('fz-upload-slot__rm-img');
     
@@ -52,9 +56,67 @@ export default function captionedImagesFileInput () {
       removeUploadedImageBtns[i].addEventListener('click', detectRemovalOfUploadedImage);
     }
     
+    const slotsEl = evt.target.parentNode.parentNode.parentNode;
+    if (typeof slotsEl.dataset.fzMaxUploadSlots !== 'undefined') {
+      addImageUploadSlot(slotsEl);
+      slotsEl.addEventListener('change', detectChangesOfUploadedImages);
+    }
+    
   }
-  
-  for (let i = 0; i < captionedImageUploadSlots.length; i++) {
-    captionedImageUploadSlots[i].addEventListener('change', detectChangesOfUploadedImages);
+
+  function getUploadSlotSnippet (slotsEl) {
+    const targetEl = slotsEl.getElementsByClassName('fz-upload-slots')[0];
+    // console.log("We are using the last .fz-upload-slot child (because it will always be empty) in the parent .fz-upload-slots element as a reference element to create upload snippets.");
+    const existingImageUploadSlotEls = targetEl.getElementsByClassName('fz-upload-slot');
+    if (existingImageUploadSlotEls.length === 0) return '';
+    const uploadSlotSnippet = existingImageUploadSlotEls[existingImageUploadSlotEls.length-1].cloneNode(true);
+    return uploadSlotSnippet;
+  }
+
+  function initialiseImageUploadSlots (slotsEl) {
+    const targetEl = slotsEl.getElementsByClassName('fz-upload-slots')[0];
+    const uploadSlotSnippet = getUploadSlotSnippet(slotsEl);
+    if (uploadSlotSnippet === '') return;
+    targetEl.innerHTML = "";
+    targetEl.appendChild(uploadSlotSnippet);
+  }
+
+  function addImageUploadSlot (slotsEl) {
+    const targetEl = slotsEl.getElementsByClassName('fz-upload-slots')[0];
+    const existingImageUploadSlotEls = targetEl.getElementsByClassName('fz-upload-slot');
+    const uploadSlotSnippet = getUploadSlotSnippet(slotsEl);
+    const maxUploadSlots = parseInt(slotsEl.dataset.fzMaxUploadSlots);
+    if (isNaN(maxUploadSlots) || maxUploadSlots === 0) return;
+    if (existingImageUploadSlotEls.length >= maxUploadSlots) return;
+    if (uploadSlotSnippet === '') return;
+    targetEl.appendChild(uploadSlotSnippet);
+  }
+
+  function removeImageUploadSlot (slotsEl, slotElMarkedForRemoval) {
+    const targetEl = slotsEl.getElementsByClassName('fz-upload-slots')[0];  
+    const existingImageUploadSlotEls = targetEl.getElementsByClassName('fz-upload-slot');
+    let previewImageGroup, visibilityOfPreviewImage, numberOfExistingImages = 0;
+    for (let i = 0; i < existingImageUploadSlotEls.length; i++) {
+      previewImageGroup = existingImageUploadSlotEls[i].getElementsByClassName('fz-upload-slot__preview-grp')[0];
+      visibilityOfPreviewImage = previewImageGroup.style.display;
+      if (visibilityOfPreviewImage !== 'none') numberOfExistingImages++;
+    }
+    const maxUploadSlots = parseInt(slotsEl.dataset.fzMaxUploadSlots);
+    if (isNaN(maxUploadSlots) || maxUploadSlots === 0) return;
+    if (numberOfExistingImages === maxUploadSlots - 1) {
+      // console.log("...adding a new empty slot since not already maxed out.");
+      const emptySlotEl = slotElMarkedForRemoval.cloneNode(true); // assuming preview image of source slotElMarkedForRemoval has aleady been cleared
+      targetEl.appendChild(emptySlotEl);
+    }
+    slotElMarkedForRemoval.remove();
+  }
+
+  // Initialising image uploaders in markup
+
+  for (let i = 0; i < captionedImageUploadSlotsEls.length; i++) {
+    captionedImageUploadSlotsEls[i].addEventListener('change', detectChangesOfUploadedImages);
+    if (typeof captionedImageUploadSlotsEls[i].dataset.fzMaxUploadSlots !== 'undefined') {
+      initialiseImageUploadSlots(captionedImageUploadSlotsEls[i]);
+    }
   }
 }
